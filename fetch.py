60f3fac4aa23c07d6b5f4dd9c23f6b5ea99c9e04
Pratyush Venkatakrishnan <contact@psvenk.com>
2023-03-18 18:57:23 -0400

    Refactor code, use connection pool

--- fetch.py
+++ fetch.py
@@ -24,18 +24,23 @@ elif "@" in password:
     print("WAREHOUSE_PASSWORD contains forbidden character '@'")
     exit(1)
 
-cp = oracledb.ConnectParams(user=username, password=password)
+pp = oracledb.PoolParams(user=username, password=password)
 dsn = "warehouse"
 del username, password
 
 # We need to use the "thick" mode so that ldap.ora and sqlplus.ora are read
 oracledb.init_oracle_client()
 
-def fetch_subject(subject_id):
-    with oracledb.connect(dsn, params=cp) as connection:
-        with connection.cursor() as cursor:
-            out = {}
+pool = oracledb.create_pool(dsn, params=pp, min=1, max=5, increment=1)
 
+def fetch_subject(subject_id):
+    """
+    Query the database and fetch the subject with the provided subject ID.
+
+    Returns None if the subject was not found.
+    """
+    with pool.acquire() as connection:
+        with connection.cursor() as cursor:
             cursor.execute("""
                 SELECT * FROM cis_course_catalog
                 WHERE subject_id = :subject_id
@@ -49,130 +54,148 @@ def fetch_subject(subject_id):
 
             row = cursor.fetchone()
 
-            if row is None:
-                print("Subject not found")
-                exit(1)
+    return process_subject(row)
 
-            out["subject_id"] = row["subject_id"]
-            out["title"] = row["subject_title"]
-            out["total_units"] = row["total_units"]
-            out["offered_fall"] = row["is_offered_fall_term"] == "Y"
-            out["offered_IAP"] = row["is_offered_iap"] == "Y"
-            out["offered_spring"] = row["is_offered_spring_term"] == "Y"
-            out["offered_summer"] = row["is_offered_summer_term"] == "Y"
-            out["public"] = True
-            out["level"] = row["hgn_code"]
+def process_subject(row):
+    """
+    Given a row from the cis_course_catalog table of the database, process it
+    into a FireRoad-compatible format.
 
-            year = int(row["academic_year"])
+    The row should be of the format returned from a SELECT * query, transformed
+    into a dict with the following lines:
 
-            if year < CURRENT_YEAR:
-                # This is considered a historical subject (for
-                # compatibility with FireRoad)
-                out["is_historical"] = True
-                out["source_semester"] = f"spring-{year}"
+        columns = [col[0].lower() for col in cursor.description]
+        cursor.rowfactory = lambda *args: dict(zip(columns, args))
 
-            if row["joint_subjects"] is not None:
-                out["joint_subjects"] = [
-                    normalize_subject_id(x)
-                    for x in row["joint_subjects"].split(",")
-                ]
+    Returns None if row is None.
+    """
+    if row is None:
+        return None
 
-            if row["equivalent_subjects"] is not None:
-                out["equivalent_subjects"] = [
-                    normalize_subject_id(x)
-                    for x in row["equivalent_subjects"].split(",")
-                ]
+    out = {}
 
-            if row["meets_with_subjects"] is not None:
-                out["meets_with_subjects"] = [
-                    normalize_subject_id(x)
-                    for x in row["meets_with_subjects"].split(",")
-                ]
+    out["subject_id"] = row["subject_id"]
+    out["title"] = row["subject_title"]
+    out["total_units"] = row["total_units"]
+    out["offered_fall"] = row["is_offered_fall_term"] == "Y"
+    out["offered_IAP"] = row["is_offered_iap"] == "Y"
+    out["offered_spring"] = row["is_offered_spring_term"] == "Y"
+    out["offered_summer"] = row["is_offered_summer_term"] == "Y"
+    out["public"] = True
+    out["level"] = row["hgn_code"]
 
-            # TODO quarter_information (term_duration)
+    year = int(row["academic_year"])
 
-            if row["is_offered_this_year"] != "Y":
-                # Not offered this year
-                out["not_offered_year"] = f"{year-1}-{year}"
+    if year < CURRENT_YEAR:
+        # This is considered a historical subject (for
+        # compatibility with FireRoad)
+        out["is_historical"] = True
+        out["source_semester"] = f"spring-{year}"
 
-            instructors = []
-            if ((out["offered_fall"] or out["offered_summer"]) and
-                    row["fall_instructors"] is not None):
-                instructors.append(f"Fall: {row['fall_instructors']}")
-            if ((out["offered_spring"] or out["offered_IAP"]) and
-                    row["spring_instructors"] is not None):
-                instructors.append(f"Spring: {row['spring_instructors']}")
-            if instructors:
-                out["instructors"] = instructors
+    if row["joint_subjects"] is not None:
+        out["joint_subjects"] = [
+            normalize_subject_id(x)
+            for x in row["joint_subjects"].split(",")
+        ]
 
-            comm_req_raw = row["comm_req_attribute"]
-            if comm_req_raw == "CIH":
-                row["communication_requirement"] = "CI-H"
-            elif comm_req_raw == "CIHW":
-                row["communication_requirement"] = "CI-HW"
-            # row["communication_requirement"] does not include CI-M
+    if row["equivalent_subjects"] is not None:
+        out["equivalent_subjects"] = [
+            normalize_subject_id(x)
+            for x in row["equivalent_subjects"].split(",")
+        ]
 
-            out["gir_attribute"] = row["gir_attribute"]
+    if row["meets_with_subjects"] is not None:
+        out["meets_with_subjects"] = [
+            normalize_subject_id(x)
+            for x in row["meets_with_subjects"].split(",")
+        ]
 
-            # TODO children, parent
+    # TODO quarter_information (term_duration)
 
-            if row["status_change"] is not None:
-                if "New number" in row["status_change"]:
-                    print("Subject has been renumbered")
-                    exit(1)
+    if row["is_offered_this_year"] != "Y":
+        # Not offered this year
+        out["not_offered_year"] = f"{year-1}-{year}"
 
-                match = re.match(
-                    r"Old number:\s+(" + SUBJECT_ID_REGEX + r")",
-                    row["status_change"])
-                if match:
-                    out["old_id"] = normalize_subject_id(match[1])
+    instructors = []
+    if ((out["offered_fall"] or out["offered_summer"]) and
+            row["fall_instructors"] is not None):
+        instructors.append(f"Fall: {row['fall_instructors']}")
+    if ((out["offered_spring"] or out["offered_IAP"]) and
+            row["spring_instructors"] is not None):
+        instructors.append(f"Spring: {row['spring_instructors']}")
+    if instructors:
+        out["instructors"] = instructors
 
-            out["lecture_units"] = row["lecture_units"]
-            out["lab_units"] = row["lab_units"]
-            out["design_units"] = row["design_units"]
-            out["preparation_units"] = row["preparation_units"]
-            out["is_variable_units"] = row["is_variable_units"] == "Y"
+    comm_req_raw = row["comm_req_attribute"]
+    if comm_req_raw == "CIH":
+        row["communication_requirement"] = "CI-H"
+    elif comm_req_raw == "CIHW":
+        row["communication_requirement"] = "CI-HW"
+    # row["communication_requirement"] does not include CI-M
 
-            # is_half_class is not populated by the FireRoad scraper and can be
-            # added via corrections
+    out["gir_attribute"] = row["gir_attribute"]
 
-            # TODO has_final
+    # TODO children, parent
 
-            out["description"] = row["subject_description"]
+    if row["status_change"] is not None:
+        if "New number" in row["status_change"]:
+            print("Subject has been renumbered")
+            exit(1)
 
-            # TODO prerequisites/corequisites
+        match = re.match(
+            r"Old number:\s+(" + SUBJECT_ID_REGEX + r")",
+            row["status_change"])
+        if match:
+            out["old_id"] = normalize_subject_id(match[1])
 
-            out["url"] = row["on_line_page_number"] + "#" + out["subject_id"]
+    out["lecture_units"] = row["lecture_units"]
+    out["lab_units"] = row["lab_units"]
+    out["design_units"] = row["design_units"]
+    out["preparation_units"] = row["preparation_units"]
+    out["is_variable_units"] = row["is_variable_units"] == "Y"
 
-            hass_attribute_raw = row["hass_attribute"]
+    # is_half_class is not populated by the FireRoad scraper and can be
+    # added via corrections
 
-            if hass_attribute_raw:
+    # TODO has_final
+
+    out["description"] = row["subject_description"]
+
+    # TODO prerequisites/corequisites
+
+    out["url"] = row["on_line_page_number"] + "#" + out["subject_id"]
+
+    hass_attribute_raw = row["hass_attribute"]
+
+    if hass_attribute_raw:
+        with pool.acquire() as connection:
+            with connection.cursor() as cursor:
                 cursor.execute("""
                     SELECT description_in_bulletin FROM cis_hass_attribute
                     WHERE hass_attribute = :hass_attribute
                 """, hass_attribute=hass_attribute_raw)
-
                 hass_res = cursor.fetchone()
-                if hass_res is not None:
-                    out["hass_attribute"] = hass_res[0]
+
+        if hass_res is not None:
+            out["hass_attribute"] = hass_res[0]
 
 
-            schedule_fall, schedule_iap, schedule_spring = (
-                fetch_schedule(cursor, subject_id, year))
-            if schedule_fall:
-                out["schedule"] = out["schedule_fall"] = schedule_fall
-            if schedule_iap:
-                out["schedule"] = out["schedule_IAP"] = schedule_iap
-            if schedule_spring:
-                out["schedule"] = out["schedule_spring"] = schedule_spring
+    schedule_fall, schedule_iap, schedule_spring = (
+        fetch_schedules(subject_id, year))
+    if schedule_fall:
+        out["schedule"] = out["schedule_fall"] = schedule_fall
+    if schedule_iap:
+        out["schedule"] = out["schedule_IAP"] = schedule_iap
+    if schedule_spring:
+        out["schedule"] = out["schedule_spring"] = schedule_spring
 
-            # TODO related_subjects
+    # TODO related_subjects
 
-            # TODO course evals
+    # TODO course evals
 
-            # TODO support corrections
+    # TODO support corrections
 
-            return out
+    return out
 
 def normalize_subject_id(subject_id):
     """
@@ -181,10 +204,10 @@ def normalize_subject_id(subject_id):
     """
     return subject_id.strip().rstrip("J")
 
-def fetch_schedule(cursor, subject_id, year):
+def fetch_schedules(subject_id, year):
     """
-    Given a cursor to the database, fetch the schedule for a subject in a
-    specified academic year (given as an int) and form a schedule string in a
+    Query the database and fetch the schedule(s) for a subject in a specified
+    academic year (given as an int) and form a schedule string in a
     standardized format.
 
     The format for a schedule is a semicolon-separated list of sections, where
@@ -221,17 +244,23 @@ def fetch_schedule(cursor, subject_id, year):
         labs = []
         designs = []
 
-        cursor.execute("""
-            SELECT meet_place, meet_time, is_lecture_section,
-                is_recitation_section, is_lab_section, is_design_section
-            FROM subject_offered
-            WHERE subject_id = :subject_id
-            AND term_code = :term_code
-            AND is_master_section = 'N'
-            ORDER BY is_lecture_section DESC, is_recitation_section DESC,
-                is_lab_section DESC, section_id
-        """, subject_id=subject_id, term_code=term_code)
-        for meet_place, meet_time, lec, rec, lab, des in cursor:
+        with pool.acquire() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT meet_place, meet_time, is_lecture_section,
+                        is_recitation_section, is_lab_section,
+                        is_design_section
+                    FROM subject_offered
+                    WHERE subject_id = :subject_id
+                    AND term_code = :term_code
+                    AND is_master_section = 'N'
+                    ORDER BY is_lecture_section DESC,
+                        is_recitation_section DESC, is_lab_section DESC,
+                        section_id
+                """, subject_id=subject_id, term_code=term_code)
+                rows = cursor.fetchall()
+
+        for meet_place, meet_time, lec, rec, lab, des in rows:
             if lec == "Y":
                 dest = lectures
             elif rec == "Y":
@@ -292,5 +321,8 @@ if __name__ == "__main__":
         exit(1)
 
     out = fetch_subject(subject_id)
+    if out is None:
+        print(f"Subject {subject_id} not found")
+        exit(1)
     for k, v in out.items():
         print(k.ljust(20) + str(v))
